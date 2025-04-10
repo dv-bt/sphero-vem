0d8cb7ec303e00d1474a9fe33f9ad29d116e8a45
dv-bt <d.bottone@pm.me>
2025-04-10 10:27:02 +0200

    Updated installation script and dependencies

--- setup.sh
+++ setup.sh
@@ -4,8 +4,12 @@
 PYTHON_VERSION="3.12.8"
 
 # Install specified Python version
-echo "Installing Python $PYTHON_VERSION..."
-pyenv install $PYTHON_VERSION
+echo "Installing Python $PYTHON_VERSION if needed..."
+if ! pyenv versions --bare | grep -q "^$PYTHON_VERSION$"; then
+    pyenv install $PYTHON_VERSION
+else
+    echo "Python $PYTHON_VERSION is already installed"
+fi
 pyenv local $PYTHON_VERSION
 
 poetry install
