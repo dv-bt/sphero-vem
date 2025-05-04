306ab7f7cc0d4b5d6d45446d104e7e9afa786bf2
dv-bt <d.bottone@pm.me>
2025-05-04 18:28:34 +0200

    Updated n2v training with new logging

--- scripts/checkpoint_explorer.py
+++ scripts/checkpoint_explorer.py
@@ -1,36 +1,20 @@
 """
-Explore what metadata is saved in n2v checkpoints
+Check timestamps of the saved model checkpoints.
 """
 
+from pathlib import Path
 import torch
-import json
+import os
+from datetime import datetime
 
 # Load the checkpoint
-checkpoint_path = "../data/models/n2v/checkpoints/last-v1.ckpt"
-checkpoint = torch.load(checkpoint_path)
-
-# Print out the keys in the checkpoint
-print("Checkpoint Keys:", checkpoint.keys())
-
-# Inspect model state_dict
-if "state_dict" in checkpoint:
-    print("\nModel State Dict:")
-    for key, tensor in checkpoint["state_dict"].items():
-        print(f"{key}: {tensor.shape}")
-
-# Inspect optimizer state_dict if available
-if "optimizer_state_dict" in checkpoint:
-    print("\nOptimizer State Dict:")
-    optimizer_state = checkpoint["optimizer_state_dict"]
-    for key, value in optimizer_state.items():
-        # Some entries in the optimizer state might be tensors, numbers, or dicts.
-        print(f"{key}: {value}")
-
-# Check additional metadata such as epoch
-if "epoch" in checkpoint:
-    print("\nCheckpoint saved at epoch:", checkpoint["epoch"])
-
-# Check which hyperparameters were saved
-if "hyper_parameters" in checkpoint:
-    hyperparams = json.dumps(checkpoint["hyper_parameters"], indent=4)
-    print(hyperparams)
+checkpoint_dir = Path("../data/models/n2v/checkpoints")
+checkpoint_list = sorted(checkpoint_dir.glob("*.ckpt"), key=os.path.getctime)
+for checkpoint_path in checkpoint_list:
+    checkpoint = torch.load(checkpoint_path)
+    creation_time = datetime.fromtimestamp(os.path.getctime(checkpoint_path))
+    modification_time = datetime.fromtimestamp(os.path.getmtime(checkpoint_path))
+    epoch = checkpoint["epoch"]
+    print(
+        f"Checkpoint {checkpoint_path.name}: epoch {epoch} saved at {creation_time}, modified at {modification_time}"
+    )
--- scripts/noise2void_train.py
+++ scripts/noise2void_train.py
@@ -5,35 +5,86 @@ import torch
 from careamics import CAREamist
 from careamics.config import create_n2v_configuration
 
-# Set lower matmul precision to use tensor cores
-torch.set_float32_matmul_precision("high")
 
-work_dir = Path("../data/models/n2v")
-work_dir.mkdir(exist_ok=True, parents=True)
+# Script variables
+# Dataset
+DATA_PATH = Path("data/raw/Au_01-vol_01")
+NUM_IMAGES = 10
+TRAIN_DIR_NAME = "n2v_training"
+VAL_SPLIT = 0.2
 
-config = create_n2v_configuration(
-    experiment_name="n2v_2D",
-    data_type="tiff",
-    axes="YX",
-    patch_size=[64, 64],
-    batch_size=128,
-    num_epochs=80,
-    logger="tensorboard",
-)
+# Hyperparameters
+BATCH_SIZE = 128
+PATCH_SIZE = 128
+EPOCHS = 100
+UNET_DEPTH = 3
+N2V2 = True
 
-careamist = CAREamist(config, work_dir=work_dir)
+# Dataloader parameters
+NUM_WORKERS = 8
 
-# Move 100 images to a separate folder for training.
-# It seems that there's no way to select a subset of images when loading
-# data, and that I cannot pass a custom dataloader.
-data_path = Path("../data/raw/Au_01-vol_01")
-train_path = data_path / "n2v_training"
-train_path.mkdir(exist_ok=True)
+# Experiment
+EXP_NAME = f"n2v-depth{UNET_DEPTH}-patch{PATCH_SIZE}-nimages{NUM_IMAGES}{'-n2v2' if N2V2 else ''}"
+WORK_DIR = Path("data/models/n2v") / EXP_NAME
 
-file_list = sorted(list(data_path.glob("*.tif")))
-for file in tqdm(file_list[:50], "Moving images to training directory..."):
-    file_dst = train_path / file.name
-    if not file_dst.exists():
+
+def configure_n2v(train_path) -> CAREamist:
+    """Create careamist object"""
+
+    WORK_DIR.mkdir(exist_ok=True, parents=True)
+
+    config = create_n2v_configuration(
+        experiment_name=EXP_NAME,
+        data_type="tiff",
+        axes="YX",
+        patch_size=[PATCH_SIZE, PATCH_SIZE],
+        batch_size=BATCH_SIZE,
+        num_epochs=EPOCHS,
+        use_n2v2=N2V2,
+        logger="wandb",
+        model_params={
+            "depth": UNET_DEPTH,
+        },
+        train_dataloader_params={"num_workers": NUM_WORKERS},
+        val_dataloader_params={"num_workers": NUM_WORKERS},
+    )
+
+    careamist = CAREamist(
+        config,
+        work_dir=WORK_DIR,
+    )
+
+    return careamist
+
+
+def prepare_dataset(data_path: Path, num_images: int, train_dir_name: str) -> Path:
+    """
+    Move first N images to a separate folder for training.
+    This removes the folder if it already exists.
+    It seems that there's no way to select a subset of images when loading
+    data, and that I cannot pass a custom dataloader.
+    """
+    train_path = data_path / train_dir_name
+    if train_path.exists():
+        shutil.rmtree(train_path)
+    train_path.mkdir()
+
+    file_list = sorted(list(data_path.glob("*.tif")))
+    for file in tqdm(file_list[:num_images], "Moving images to training directory..."):
         shutil.copy(file, train_path / file.name)
 
-careamist.train(train_source=train_path, use_in_memory=False, val_minimum_split=5)
+    return train_path
+
+
+def train_n2v():
+    train_path = prepare_dataset(DATA_PATH, NUM_IMAGES, TRAIN_DIR_NAME)
+    careamist = configure_n2v(train_path)
+    careamist.train(
+        train_source=train_path, use_in_memory=False, val_percentage=VAL_SPLIT
+    )
+
+
+if __name__ == "__main__":
+    # Set lower matmul precision to use tensor cores
+    torch.set_float32_matmul_precision("high")
+    train_n2v()
