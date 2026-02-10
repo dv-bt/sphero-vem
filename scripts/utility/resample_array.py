a31d419665c6510deea86a9c8b2041399f71ddb5
dv-bt <d.bottone@pm.me>
2026-02-10 15:36:28 +0100

    Updated resample script

--- scripts/utility/resample_array.py
+++ scripts/utility/resample_array.py
@@ -9,9 +9,9 @@ from sphero_vem.preprocessing import resample_array
 def main():
     """Resample zarr array"""
     root_path = Path("data/processed/segmented/Au_01-vol_01.zarr")
-    target_spacing = (50, 25, 25)
-    array_path = "labels/nps/posterior/50-10-10"
-    resample_array(root_path, array_path, target_spacing)
+    target_spacing = (100, 100, 100)
+    array_path = "images/50-50-50"
+    resample_array(root_path, array_path, target_spacing, n_workers=8)
 
 
 if __name__ == "__main__":
